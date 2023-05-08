c4e625ffecc15bf437c36514ab30b910c1c2cbfd
Amaey Mittal <100258151+amaeymittal@users.noreply.github.com>
2023-05-08 15:40:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+sound = ""
+function preload(){
+    sound = loadSound("music.mp3")
+}
+
+function setup(){
+    canvas = createCanvas(600,500)
+    canvas.center();
+
+    video = createCapture(VIDEO)
+    video.hide()
+
+}
+function draw(){
+    image(video,0,0,600,500)
+}
+function play(){
+    sound.play()
+}
